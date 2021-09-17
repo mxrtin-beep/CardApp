1ee465a153f8f70349ab1f9d15098cf680c7e492
Martin  Bourdev <martinbourdev@Martins-Air.attlocal.net>
2021-09-17 14:57:35 -0700

    1: Files

---
+++ Chinese/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chinese
+//
+//  Created by Martin  Bourdev on 9/17/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
